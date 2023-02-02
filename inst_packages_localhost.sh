0c3548d28c752862d67606e1e28797f3f55b904a
throttlemeister <throttlemeister@sport-touring.eu>
2023-02-02 22:14:53 +0100

    Small update to check and install ansible first before attempting to install anything else...

--- inst_packages_localhost.sh
+++ inst_packages_localhost.sh
@@ -2,12 +2,32 @@
 
 # Check if Ansible is installed
 if ! command -v ansible-playbook > /dev/null; then
-  echo "Ansible is not installed. Please install Ansible first."
-  exit 1
+  echo "Ansible is not installed. Installing Ansible now..."
+
+  # Determine the package manager to use for installing Ansible
+  if command -v dnf > /dev/null; then
+    package_manager="dnf install -y"
+  elif command -v apt-get > /dev/null; then
+    package_manager="apt-get install -y"
+  else
+    echo "Neither dnf nor apt-get was found. Please install one of them."
+    exit 1
+  fi
+
+  # Install Ansible
+  $package_manager ansible
 fi
 
 # Define the list of packages to install
 packages=(btop git lolcat figlet cowsay fish conky inxi timeshift alacritty cpu-x digikam exa ripgrep kalendar kdenlive kmail krita mc neofetch rawtherapee)
+
+# Create an Ansible playbook
+cat > install-packages.yml << EOL
+- name: Install Packages
+  hosts: localhost
+  become: yes
+  tasks:
+EOL
 
 # Determine the package manager to use
 if command -v dnf > /dev/null; then
@@ -18,14 +38,6 @@ else
   echo "Neither dnf nor apt-get was found. Please install one of them."
   exit 1
 fi
-
-# Create an Ansible playbook
-cat > install-packages.yml << EOL
-- name: Install Packages
-  hosts: localhost
-  become: yes
-  tasks:
-EOL
 
 # Add tasks for each package
 for package in "${packages[@]}"; do
