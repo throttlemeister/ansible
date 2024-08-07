567caed5c802675641124ac5031a4c33a9f6f2d0
throttlemeister <throttlemeister@sport-touring.eu>
2024-08-07 14:22:27 +0200

    Added install / setup script specifically for OpenSUSE

---
+++ inst_packages_localhost_opensuse.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+
+# User to setup profile and other files for
+SETUP_USER=throttlemeister
+
+# Check if Ansible is installed
+if ! command -v ansible-playbook > /dev/null; then
+  echo "Ansible is not installed. Installing Ansible now..."
+  # Install Ansible
+  sudo zypper in -y ansible
+  sudo zypper in -y opi
+fi
+
+# Define the list of packages to install
+packages=(btop git lolcat figlet cowsay fish conky inxi alacritty cpu-x digikam eza ripgrep kalendar kdenlive kmail krita mc fastfetch rawtherapee ktorrent)
+
+# Create an Ansible playbook
+cat > install-packages.yml << EOL
+- name: Install Packages
+  hosts: localhost
+  become: yes
+  tasks:
+EOL
+
+# Add tasks for each package
+for package in "${packages[@]}"; do
+  cat >> install-packages.yml << EOL
+    - name: Install $package
+      package:
+        name: $package
+        state: present
+EOL
+done
+
+# Run the Playbook
+ansible-playbook install-packages.yml
+
+# Clean up the playbook file when we are done
+rm install-packages.yml
+
+# Setup profile
+cd /home/$SETUP_USER
+tar xvfz /home/backup/profile_proper.tar.gz
+
+# Clone fish configuration and ansible
+cd /home/$SETUP_USER/.config
+git clone git@github.com:throttlemeister/fish.git
+cd /home/$SETUP_USER/
+git clone git@github.com:throttlemeister/ansible.git
