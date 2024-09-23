083cd8f571269c3c17bd3395bcb1e8d9ea9f9e00
throttlemeister <throttlemeister@sport-touring.eu>
2024-09-23 19:43:43 +0200

    Added / changed scripts and files specifically for opensuse, as we are currently running opensuse only, so no need to make things more complex than needed by accommodating other operating systems.

--- prep_opensuse_desktop.sh
+++ prep_opensuse_desktop.sh
@@ -19,41 +19,6 @@ fi
 # Run the Ansible playbook to install standard packages
 ansible-playbook files/install_local_packages.yml
 
-### START OF OLD STUFF - TO BE REMOVED
-
-## Define the list of packages to install
-#packages=(opi stow neovim btop git lolcat figlet cowsay fish conky inxi alacritty cpu-x digikam eza ripgrep kalendar kdenlive kmail krita mc fastfetch rawtherapee ktorrent)
-
-## Create an Ansible playbook
-#cat >install-packages.yml <<EOL
-#- name: Install Packages
-#  hosts: localhost
-#  become: yes
-#  tasks:
-#EOL
-
-## Add tasks for each package
-#for package in "${packages[@]}"; do
-#  cat >>install-packages.yml <<EOL
-#    - name: Install $package
-#      package:
-#        name: $package
-#        state: present
-#EOL
-#done
-
-## Run the Playbook
-#ansible-playbook install-packages.yml
-
-## Clean up the playbook file when we are done
-#rm install-packages.yml
-
-## Install MS Edge and VS Code from OPI
-#opi -n msedge
-#opi -n vscode
-
-### END OF OLD stuff
-
 # Clone dotfiles repo and use stow to setup the rest of the stuff
 cd /home/$SETUP_USER/
 mkdir .dotfiles
